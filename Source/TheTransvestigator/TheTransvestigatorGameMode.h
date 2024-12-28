046073972626d40447eda5df6f7166af9bdd3ee1
Paulpoomrit <paulpoomrit@gmail.com>
2024-12-27 16:58:01 -0800

    Initial commit

---
+++ Source/TheTransvestigator/TheTransvestigatorGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheTransvestigatorGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATheTransvestigatorGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATheTransvestigatorGameMode();
+};
+
+
+
